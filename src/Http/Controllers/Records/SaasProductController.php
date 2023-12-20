64247c1be0c398f542e256e0eac5de348a1d4d20
smdm22 <smdm20@gmail.com>
2023-12-20 16:45:04 +0330

    feat: added SaasProductController for fetching product from api

---
+++ src/Http/Controllers/Records/SaasProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Smdm\SaasCrm\Http\Controllers\Records;
+
+use GuzzleHttp\Client;
+
+use Smdm\SaasCrm\Http\Controllers\Auth\SaasTokenCheck;
+
+class SaasProductController
+{
+
+   
+    public static function search($phrase)
+    {
+        $token = SaasTokenCheck::getToken();
+       
+        if (!$token) {
+            return null;
+        }
+        
+        $client = new Client([
+            'base_uri' => config('saas-crm.saas_crm_api_base_url'),
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+
+        try {
+            
+            $response = $client->request('POST', rtrim(config('saas-crm.saas_crm_api_version'), '/') . '/product/search', [
+                'json' => ['query' => $phrase]
+            ]);
+
+            return json_decode($response->getBody(), true);
+        } catch (\Exception $e) {
+            // Handle the exception or log it
+            return null; // or return a meaningful error message
+        }
+    }
+
+}
+
--- src/SaasCrm.php
+++ src/SaasCrm.php
@@ -2,6 +2,13 @@
 
 namespace Smdm\SaasCrm;
 
+use Smdm\SaasCrm\Http\Controllers\Records\SaasProductController;
+
 class SaasCrm
 {
+
+    public static function productsSearch($phrase)
+    {
+        return SaasProductController::search($phrase);
+    }
 }
