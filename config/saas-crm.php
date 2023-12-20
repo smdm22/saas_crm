62fd1f0c69c3aa8e008deb543fffc52ec80aacf3
smdm22 <smdm20@gmail.com>
2023-12-20 16:44:22 +0330

    feat: added needed .env param in config

--- config/saas-crm.php
+++ config/saas-crm.php
@@ -2,5 +2,8 @@
 
 // config for Smdm/SaasCrm
 return [
+    'saas_crm_api_key' => env('SAAS_CRM_API_KEY', ''), 
+    'saas_crm_api_version'=>env('SAAS_CRM_API_VERSION','/api/v1'),
+    'saas_crm_api_base_url'=>env('SAAS_CRM_API_BASE_URL','http://127.0.0.1:8000'),
 
 ];
