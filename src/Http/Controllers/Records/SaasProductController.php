f6af1ce5c5835a7359114b7cdbc670efe640d5d7
smdm22 <smdm22@users.noreply.github.com>
2023-12-20 13:16:04 +0000

    Fix styling

--- src/Http/Controllers/Records/SaasProductController.php
+++ src/Http/Controllers/Records/SaasProductController.php
@@ -1,35 +1,31 @@
 <?php
 
-
 namespace Smdm\SaasCrm\Http\Controllers\Records;
 
 use GuzzleHttp\Client;
-
 use Smdm\SaasCrm\Http\Controllers\Auth\SaasTokenCheck;
 
 class SaasProductController
 {
-
-   
     public static function search($phrase)
     {
         $token = SaasTokenCheck::getToken();
-       
-        if (!$token) {
+
+        if (! $token) {
             return null;
         }
-        
+
         $client = new Client([
             'base_uri' => config('saas-crm.saas_crm_api_base_url'),
             'headers' => [
-                'Authorization' => 'Bearer ' . $token,
+                'Authorization' => 'Bearer '.$token,
             ],
         ]);
 
         try {
-            
-            $response = $client->request('POST', rtrim(config('saas-crm.saas_crm_api_version'), '/') . '/product/search', [
-                'json' => ['query' => $phrase]
+
+            $response = $client->request('POST', rtrim(config('saas-crm.saas_crm_api_version'), '/').'/product/search', [
+                'json' => ['query' => $phrase],
             ]);
 
             return json_decode($response->getBody(), true);
@@ -38,6 +34,4 @@ class SaasProductController
             return null; // or return a meaningful error message
         }
     }
-
 }
-
--- src/SaasCrm.php
+++ src/SaasCrm.php
@@ -6,7 +6,6 @@ use Smdm\SaasCrm\Http\Controllers\Records\SaasProductController;
 
 class SaasCrm
 {
-
     public static function productsSearch($phrase)
     {
         return SaasProductController::search($phrase);
