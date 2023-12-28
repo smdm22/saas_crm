9d471cc1876b5fdb58fd3b586871ebc5e248204f
smdm22 <smdm20@gmail.com>
2023-12-28 09:12:08 +0330

    feat: adding availabilitySearchById and regular search for SaasAvailabilityController

---
+++ src/Http/Controllers/Records/SaasAvailabilityController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Smdm\SaasCrm\Http\Controllers\Records;
+
+use GuzzleHttp\Client;
+use Smdm\SaasCrm\Http\Controllers\Auth\SaasTokenCheck;
+
+class SaasAvailabilityController
+{
+    public static function search($phrase)
+    {
+        $token = SaasTokenCheck::getToken();
+
+        if (! $token) {
+            return null;
+        }
+
+        $client = new Client([
+            'base_uri' => config('saas-crm.saas_crm_api_base_url'),
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        try {
+
+            $response = $client->request('POST', rtrim(config('saas-crm.saas_crm_api_version'), '/').'/availability/search', [
+                'json' => ['query' => $phrase],
+            ]);
+
+            return json_decode($response->getBody(), true);
+        } catch (\Exception $e) {
+            // Handle the exception or log it
+            return null; // or return a meaningful error message
+        }
+    }
+
+
+    public static function availabilitySearchById($availability_id)
+    {
+        $token = SaasTokenCheck::getToken();
+
+        if (! $token) {
+            return null;
+        }
+
+        $client = new Client([
+            'base_uri' => config('saas-crm.saas_crm_api_base_url'),
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        try {
+
+            $response = $client->request('POST', rtrim(config('saas-crm.saas_crm_api_version'), '/').'/availability/search', [
+                'json' => ['availability_id' => $availability_id],
+            ]);
+
+            return json_decode($response->getBody(), true);
+        } catch (\Exception $e) {
+            // Handle the exception or log it
+            return null; // or return a meaningful error message
+        }
+
+    }
+
+ 
+}
