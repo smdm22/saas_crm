e45e8862309f835c47004ccd5e8a783ab92ab513
davoodf1995 <davood.thinker@gmail.com>
2024-01-10 12:20:28 +0330

    feat: add create single excess api

--- src/Http/Controllers/Records/SaasExcessController.php
+++ src/Http/Controllers/Records/SaasExcessController.php
@@ -119,4 +119,31 @@ class SaasExcessController
             return null;
         }
     }
+
+    public static function createSingleExcess($data = [])
+    {
+        $token = SaasTokenCheck::getToken();
+
+        if (! $token) {
+            return null;
+        }
+
+        $client = new Client([
+            'base_uri' => config('saas-crm.saas_crm_api_base_url'),
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        try {
+            $response = $client->request('POST', rtrim(config('saas-crm.saas_crm_api_version'), '/').'/excess', [
+                'json' => $data,
+            ]);
+
+            return json_decode($response->getBody(), true);
+        } catch (\Exception $e) {
+            // Consider logging the exception or handling it as needed
+            return $e->getMessage();
+        }
+    }
 }
